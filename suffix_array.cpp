73a8650567faa66a81961116e561539f73959884
chayan kumar roy <31990702+chayankumar999@users.noreply.github.com>
2018-08-28 15:31:13 +0600

    SuffixArray+KMP+BinarySearch (LOJ 1428)

--- suffix_array.cpp
+++ suffix_array.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #define MX 100005
 #define mem(a) memset(a,0,sizeof(a))
+#define sc(a) scanf("%d",&a)
 struct SUFFIX_ARRAY
 {
     int ra[MX],tra[MX],sa[MX],tsa[MX],r[MX];
@@ -47,7 +48,7 @@ struct SUFFIX_ARRAY
         for(i=0; i<n; lcp[rank[i++]] = k )
             for(k?k--:0,j=sa[rank[i]-1]; r[i+k]==r[j+k]; k++);
     }
-    void generate(char *s)
+    void generate(string s)
     {
         int n;
         for (n=0; s[n] ; n++)
@@ -70,13 +71,80 @@ struct SUFFIX_ARRAY
         cout << endl;
     }
 } SA;
+struct _KMP
+{
+    int lps[MX],a[MX],c;
+    void LpsArray(string st)
+    {
+        int i,j;
+        i=0; j=1;
+        while(st[j])
+        {
+            if(st[i]==st[j]) lps[j++]=++i;
+            else
+                if(i==0) lps[j++]=i;
+                else  i=lps[i-1];
+        }
+    }
+    void KmpSearch(string s, string st)
+    {
+        int i,j;
+        i=j=c=0;
+        while(s[i])
+        {
+            if(s[i]==st[j]) {i++,j++;
+                if(j==st.size()) a[c++]=i-j, j=lps[j-1];}
+            else
+                if(j!=0) j=lps[j-1];
+                else i++;
+        }
+        a[c++]=s.size();
+    }
+    int BinarySearch(int v)
+    {
+        int lo,hi,md;
+        lo=0;hi=c-1;
+        while(lo<=hi)
+        {
+            md=(lo+hi)/2;
+            if(a[md]==v) {lo=md;break;}
+            else if(a[md]>v) hi=md-1;
+            else lo=md+1;
+        }
+        return lo;
+    }
+
+}KMP;
 int main()
 {
-    int n,m,i,j,k;
-    char s[MX];
-    scanf("%s",s); // scan as char array not string
-    SA.generate(s);
-    SA.print();
+    int n,m,i,j,k,tc,cs=1,x,y,z,cnt;
+    string s,st;
+    sc(tc);
+    while(tc--)
+    {
+        cin>>s>>st;
+        SA.generate(s);
+        KMP.LpsArray(st);
+        KMP.KmpSearch(s,st);
+        cnt=0;
+        for(i=1;i<=SA.len;i++)
+        {
+            int _lcp=SA.lcp[i];
+            x=SA.sa[i];
+            int lo=KMP.BinarySearch(x);
+            y=KMP.a[lo];
+//            if(y!=s.size())
+//            {
+//                y+=st.size()-2;
+//            }
+            x=y-x;
+            if(x>_lcp)
+            {
+                cnt+=x-_lcp;
+            }
+        }
+        cout<<cnt<<endl;
+    }
     return 0;
 }
 
