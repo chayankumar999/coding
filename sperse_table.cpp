ef85ab91970f5a068abece4049794d6b2d2119fb
chayan kumar roy <chayankumar999@gmail.com>
2019-07-30 06:17:49 +0600

    Update sperse_table.cpp

--- sperse_table.cpp
+++ sperse_table.cpp
@@ -1,57 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define mx 12345678
-ll n;
-ll a[100005],sp[100001][50];
-void memSet()
+#define FasterIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define mem(a,x) memset(a,x,sizeof(a))
+#define pb push_back
+#define vi vector<int>
+
+const int MX=200000;
+const int LG=18;
+
+int sp[MX+5][LG+2];
+
+void SPTBL(int n)
 {
-    for(ll i=0; i<=n+1; i++)
+    for(int i=1; i<=n; i++)
     {
-        for(ll j=0; j<=log2(n)+1; j++)
+        for(int j=1; (1<<j)<=n; j++)
         {
-            sp[i][j]=1234567;
+            if(sp[i][j-1]==-1) continue;
+            sp[i][j]=sp[sp[i][j-1]][j-1];
         }
     }
 }
+
 int main()
 {
-    ll i,j,k,l,m,tc,t=1;
-    scanf("%lld",&tc);
-    while(tc--)
-    {
-        scanf("%lld%lld",&n,&m);
-        memSet();
-        for(i=1; i<=n; i++)
-        {
-            scanf("%lld",&a[i]);
-            sp[i][0]=a[i];
-        }
-        for(i=1; i<=log2(n); i++)
-        {
-            k=pow(2,i-1);
-            for(j=1; j<=n; j++)
-            {
-                if(k+j>=n)
-                {
-                    sp[j][i]=min(sp[j][i-1],sp[n][i-1]);
-                    continue;
-                }
-                sp[j][i]=min(sp[j][i-1],sp[j+k][i-1]);
-            }
-        }
-        printf("Case %lld:\n",t++);
-        for(i=0; i<m; i++)
-        {
-            ll x,y;
-            scanf("%lld%lld",&x,&y);
-            ll d1=y;
-            ll d=y-x+1;
-            y=log2(d);
-            d=d1-pow(2,y)+1;
-            ll mnm=min(sp[x][y],sp[d][y]);
-            printf("%lld\n",mnm);
-        }
-    }
+    FasterIO;
+    int n,q;
+    SPTBL(n);
     return 0;
 }
