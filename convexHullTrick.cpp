b1d1f2ba85587da3a5b6a1695523a7629abc5473
chayan kumar roy <chayankumar999@gmail.com>
2019-08-02 01:04:25 +0600

    Update convexHullTrick.cpp

--- convexHullTrick.cpp
+++ convexHullTrick.cpp
@@ -161,7 +161,8 @@ int main()
     for(int i=0; i<n; i++)
     {
         dp[i]=st.getbest(y[i])+a[i];
-        st.add_line(-x[i],dp[i]);
+        st.add_line(-x[i],dp[i]); // for maximum
+       //st.add_line(x[i],dp[i]); // for minimum
     }
     ll ans=0;
     for(int i=0; i<n; i++)
@@ -171,3 +172,180 @@ int main()
     cout<<ans<<endl;
     return 0;
 }
+
+
+
+///Online update and online query
+///everything is fine as long as you want the minimum/maximum
+
+///https://codeforces.com/problemset/problem/1083/E
+
+#include <bits/stdc++.h>
+using namespace std;
+#define FasterIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+typedef long long ll;
+struct line
+{
+    long long a, b;
+    double xleft;
+    bool type;
+    line(long long _a, long long _b)
+    {
+        a = _a;
+        b = _b;
+        type = 0;
+    }
+    bool operator < (const line &other) const
+    {
+        if(other.type)
+        {
+            return xleft < other.xleft;
+        }
+        return a > other.a;
+    }
+};
+double meet(line x, line y)
+{
+    return 1.0 * (y.b - x.b) / (x.a - y.a);
+}
+struct cht
+{
+    set < line > hull;
+    cht()
+    {
+        hull.clear();
+    }
+    typedef set < line > :: iterator ite;
+    bool hasleft(ite node)
+    {
+        return node != hull.begin();
+    }
+    bool hasright(ite node)
+    {
+        return node != prev(hull.end());
+    }
+    void updateborder(ite node)
+    {
+        if(hasright(node))
+        {
+            line temp = *next(node);
+            hull.erase(temp);
+            temp.xleft = meet(*node, temp);
+            hull.insert(temp);
+        }
+        if(hasleft(node))
+        {
+            line temp = *node;
+            temp.xleft = meet(*prev(node), temp);
+            hull.erase(node);
+            hull.insert(temp);
+        }
+        else
+        {
+            line temp = *node;
+            hull.erase(node);
+            temp.xleft = -1e18;
+            hull.insert(temp);
+        }
+    }
+    bool useless(line left, line middle, line right)
+    {
+        double x = meet(left, right);
+        double y = x * middle.a + middle.b;
+        double ly = left.a * x + left.b;
+        return y > ly;
+    }
+    bool useless(ite node)
+    {
+        if(hasleft(node) && hasright(node))
+        {
+            return useless(*prev(node), *node, *next(node));
+        }
+        return 0;
+    }
+    void add_line(long long a, long long b)
+    {
+        line temp = line(-a, -b); // for maximum
+        //line temp = line(a, b); // for minimum
+        auto it = hull.lower_bound(temp);
+        if(it != hull.end() && it -> a == a)
+        {
+            if(it -> b > b)
+            {
+                hull.erase(it);
+            }
+            else
+            {
+                return;
+            }
+        }
+        hull.insert(temp);
+        it = hull.find(temp);
+        if(useless(it))
+        {
+            hull.erase(it);
+            return;
+        }
+        while(hasleft(it) && useless(prev(it)))
+        {
+            hull.erase(prev(it));
+        }
+        while(hasright(it) && useless(next(it)))
+        {
+            hull.erase(next(it));
+        }
+        updateborder(it);
+    }
+    long long getbest(long long x)
+    {
+        if(hull.empty())
+        {
+            return 1e18;
+        }
+        line query(0, 0);
+        query.xleft = x;
+        query.type = 1;
+        auto it = hull.lower_bound(query);
+        it = prev(it);
+        return -(it -> a * x + it -> b); // for maximum
+        //return (it -> a * x + it -> b); // for minimum
+    }
+}st;
+const int MX=2000000;
+ll x[MX],y[MX],a[MX],dp[MX];
+vector<pair<pair<ll,ll>, ll> >v;
+int main()
+{
+    FasterIO;
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++)
+    {
+        ll x,y,a;
+        cin>>x>>y>>a;
+        v.push_back({{x,y},a});
+    }
+    sort(v.begin(),v.end());
+    for(int i=0; i<n; i++)
+    {
+        x[i]=v[i].first.first;
+        y[i]=v[i].first.second;
+        a[i]=v[i].second;
+        a[i]=x[i]*y[i]-a[i];
+    }
+    st.add_line(0,0);
+    for(int i=0; i<n; i++)
+    {
+        dp[i]=st.getbest(y[i])+a[i];
+        st.add_line(-x[i],dp[i]); // for maximum
+       //st.add_line(x[i],dp[i]); // for minimum
+    }
+    ll ans=0;
+    for(int i=0; i<n; i++)
+    {
+        ans=max(ans,dp[i]);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
+
