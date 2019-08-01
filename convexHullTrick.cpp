1051a7bd2387ff78a591085a85357fb7a34aba09
chayan kumar roy <chayankumar999@gmail.com>
2019-08-02 00:20:42 +0600

    Update convexHullTrick.cpp

--- convexHullTrick.cpp
+++ convexHullTrick.cpp
@@ -70,3 +70,105 @@ int main()
     cout<<ans<<endl;        //printing dp[n-1]
     return 0;
 }
+
+
+
+///Online update and online query
+///everything is fine as long as you want the minimum/maximum
+
+///https://codeforces.com/problemset/problem/1083/E
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FasterIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+typedef long long ll;
+typedef long double float128;
+const ll is_query = -(1LL<<62), inf = 1e18;
+struct Line
+{
+    ll m, b;
+    mutable function<const Line*()> succ;
+    bool operator<(const Line& rhs) const
+    {
+        if (rhs.b != is_query) return m < rhs.m;
+        const Line* s = succ();
+        if (!s)  return 0;
+        ll x = rhs.m;
+        return b - s->b < (s->m - m) * x;
+    }
+};
+
+struct HullDynamic : public multiset<Line> // // will maintain lower hull for minimum/maximum
+{
+    bool bad(iterator y)
+    {
+        auto z = next(y);
+        if (y == begin())
+        {
+            if (z == end())       return 0;
+            return y->m == z->m && y->b <= z->b;
+        }
+        auto x = prev(y);
+        if (z == end()) return y->m == x->m && y->b <= x->b;
+        return (float128)(x->b - y->b)*(z->m - y->m) >= (float128)(y->b - z->b)*(y->m - x->m);
+    }
+    void add_line(ll m, ll b)
+    {
+        //auto y = insert({ -m, -b });   // For minimum
+        auto y = insert({ m, b });       // For maximum
+        y->succ = [=] { return next(y) == end() ? 0 : &*next(y); };
+        if (bad(y))
+        {
+            erase(y); return;
+        }
+        while (next(y) != end() && bad(next(y)))    erase(next(y));
+        while (y != begin() && bad(prev(y)))        erase(prev(y));
+    }
+
+    ll getbest(ll x)
+    {
+        auto l = *lower_bound((Line)
+        {
+            x, is_query
+        });
+        //return -(l.m * x + l.b);  // For minimum
+        return (l.m * x + l.b);     // For maximum
+    }
+} st;
+
+const int MX=2000000;
+ll x[MX],y[MX],a[MX],dp[MX];
+vector<pair<pair<ll,ll>, ll> >v;
+int main()
+{
+    FasterIO;
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++)
+    {
+        ll x,y,a;
+        cin>>x>>y>>a;
+        v.push_back({{x,y},a});
+    }
+    sort(v.begin(),v.end());
+    for(int i=0; i<n; i++)
+    {
+        x[i]=v[i].first.first;
+        y[i]=v[i].first.second;
+        a[i]=v[i].second;
+        a[i]=x[i]*y[i]-a[i];
+    }
+    st.add_line(0,0);
+    for(int i=0; i<n; i++)
+    {
+        dp[i]=st.getbest(y[i])+a[i];
+        st.add_line(-x[i],dp[i]);
+    }
+    ll ans=0;
+    for(int i=0; i<n; i++)
+    {
+        ans=max(ans,dp[i]);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
