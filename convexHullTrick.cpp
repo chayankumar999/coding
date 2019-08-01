803896e856142f9f0819b19f6c0ad559cf5bf6c9
chayan kumar roy <chayankumar999@gmail.com>
2019-08-01 22:49:02 +0600

    Update convexHullTrick.cpp

--- convexHullTrick.cpp
+++ convexHullTrick.cpp
@@ -8,6 +8,9 @@
     cost will be f[i]=f[j]+a[i]*b[j] , the cost for cutting the nth tree will be the final answer.
 */
 
+//min query increasing slope
+//or max query decreasing slope
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -18,51 +21,52 @@ typedef long long ll;
 typedef pair<ll,ll> pL;
 
 const int MX=200000;
+const ll inf=1e9;
+double interP[MX];     //intersection point of corresponding line
+pL st[MX];             //lines will be stored here in sorted order
+int top;
 
 double intersect(pL a, pL b)
 {
     return (b.ss-a.ss+0.0)/(a.ff-b.ff);
 }
-//void insert(pL candidate, pL *st, double *interP, int &top)
-//{
-//    while(top>0 && intersect(st[top-1],st[top])>= intersect(st[top-1],candidate)) top--;
-//    st[++top]=candidate;
-//    interP[top]=intersect(st[top-1],candidate);
-//}
-//ll Q(ll x, pL *st, double *interP, int top)
-//{
-//    int up=upper_bound(interP+1,interP+top+1,x)-interP-1;
-//    return st[up].ff*(x)+st[up].ss;
-//}
-//void init(pL candidate, double base, pL *st, double *interP, int &top)
-//{
-//    top=0;
-//    st[0]=candidate;
-//    interP[0]=base;
-//}
+void insert(pL candidate)
+{
+    while(top>0 && intersect(st[top-1],st[top])>= intersect(st[top-1],candidate)) top--;
+    st[++top]=candidate;
+    interP[top]=intersect(st[top-1],candidate);
+}
+ll Q(ll x)
+{
+    int up=upper_bound(interP+1,interP+top+1,x)-interP-1;
+    return st[up].ff*(x)+st[up].ss;
+}
+
+//for minimam candidate = {inf,0}, for maximum candidate = {-inf,0}
+
+void init(pL candidate={inf,0}, double base = -inf)
+{
+    top=0;
+    st[0]=candidate;
+    interP[0]=base;
+    insert({0,0});    //inserting {0,dp[0]}
+}
 ll a[MX],b[MX];
-double interP[MX];
-pL st[MX];
 
 int main()
 {
-    int n,top;
+    int n;
     cin>>n;
     for(int i=1;i<=n;i++) cin>>a[i];
     for(int i=1;i<=n;i++) cin>>b[i];
-    top=0;         st[0]={b[1],0};  //initialize
-    interP[0]=0;   ll ans=0;
-    // we assume tree height a[i] as x, b[i] as m and ans as c for line equation y=mx+c
-    // line 57-59 is insertion, 54-55 query
-    for(int i=2;i<=n;i++)
+    ll ans=0;
+    init();             //Initial set up
+    //y = m * x + c, for insert send {m,c}
+    for(int i=1;i<=n;i++)
     {
-        int up=upper_bound(interP+1,interP+top+1,a[i])-interP-1;
-        ans=st[up].ff*a[i]+st[up].ss;
-        pL candidate={b[i],ans};
-        while(top>0 && intersect(st[top-1],st[top])>= intersect(st[top-1],candidate)) top--;
-        st[++top]=candidate;
-        interP[top]=intersect(st[top-1],candidate);
+        ans=Q(a[i]);        //query on point a[i]
+        insert({b[i],ans}); //insert {b[i],dp[i]}
     }
-    cout<<ans<<endl;
+    cout<<ans<<endl;        //printing dp[n-1]
     return 0;
 }
