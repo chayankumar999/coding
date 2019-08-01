0608c733a83f68d461450a728aca7c6a2bba6dfb
chayan kumar roy <chayankumar999@gmail.com>
2019-08-01 17:43:28 +0600

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -26,11 +26,11 @@ void dfs(int x, int p)
         dfs(u,x);
     }
 }
-void SPTBL(int n)
+void SPTBL()
 {
-    for(int i=1; i<=n; i++)
+    for(int j=1; (1<<j)<=n; j++)
     {
-        for(int j=1; (1<<j)<=n; j++)
+        for(int i=1; i<=n; i++)
         {
             if(sp[i][j-1]==-1) continue;
             sp[i][j]=sp[sp[i][j-1]][j-1];
