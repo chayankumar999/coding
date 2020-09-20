12154a1b65327546c061f865dd90d5af84e6949d
chayan kumar roy <chayankumar999@gmail.com>
2020-09-21 04:18:55 +0600

    articulation bridge

--- bridge.cpp
+++ bridge.cpp
@@ -1,109 +1,98 @@
+/*
+Problem: Real Life Traffic (Light Oj)
+Problem Link: http://lightoj.com/volume_showproblem.php?problem=1291
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define db double
-#define pb push_back
-#define mk make_pair
-#define pi 2*acos(0.0)
-#define pf(a) printf("%lld\n",a)
-#define sc(a) scanf("%lld",&a)
-#define scn(a,b) scanf("%lld%lld",&a,&b)
-#define pf1(t,a) printf("Case %lld: %lld\n",t,a)
-#define mem(a) memset(a,0,sizeof (a))
-#define mems(a) memset(a,-1,sizeof(a))
-vector<int>v[100005];
-int ans[10005],vis[100005];
-int low[10005],dis[100005],n,m,c,p;
-vector< pair<int,int> >pr;
-void dfs(int x,int p)
+
+const int MX=10005;
+
+vector<int> g[MX];
+
+vector<pair<int, int> > E;
+
+int vis[MX], low[MX], dis[MX];
+int f[MX], component[MX], Time;
+
+map<pair<int, int>, int>mp;
+
+void CLEAR(int n)
 {
-    vis[x]=1;
-    int sz=v[x].size();
-    low[x]=dis[x]=++c;
-    for(int i=0; i<sz; i++)
+    Time=0; mp.clear(); E.clear();
+    for(int i=0;i<=n;i++)
     {
-        int adx=v[x][i];
-        if(adx==p)
+        vis[i]=low[i]=dis[i]=f[i]=0;
+        g[i].clear();
+    }
+}
+
+void dfs(int u, int p)
+{
+    low[u]=dis[u]=++Time;
+    for(auto v:g[u])
+    {
+        if(v==p) continue;
+
+        if(low[v]==0)
         {
-            continue;
-        }
-        if(low[adx]==0)
-        {
-            dfs(adx,x);
-            if(dis[x]<low[adx])
+            dfs(v,u);
+            if(dis[u]<low[v])
             {
-                if(x<adx)
-                {
-                    pr.pb(mk(x,adx));
-                }
-                else
-                {
-                    pr.pb(mk(adx,x));
-                }
+                mp[{u,v}]=mp[{v,u}]=1; E.push_back({u,v});
             }
-            low[x]=min(low[x],low[adx]);
+            low[u]=min(low[u], low[v]);
         }
         else
         {
-            low[x]=min(low[x],dis[adx]);
+            low[u]=min(low[u], dis[v]);
         }
     }
 }
+
+void dfs2(int u, int id)
+{
+    vis[u]=1; component[u]=id;
+    for(auto v:g[u])
+    {
+        if(vis[v] ||mp.count({u, v}) )continue;
+        dfs2(v, id);
+    }
+}
+
 int main()
 {
-    int i,j,k,l,x,y,tc,t=1,z;
-    char s[100005];
-    scanf("%d",&tc);
+    int tc, cs=1; scanf("%d", &tc);
     while(tc--)
     {
-        scanf("%d",&n);
-        for(i=0; i<n; i++)
+        int n, m; scanf("%d%d", &n, &m); CLEAR(n);
+
+        for(int i=1; i<=m; i++)
         {
-            scanf("%d%s",&x,s);
-            z=0;
-            for(j=1; j<strlen(s)-1; j++)
-            {
-                z=z*10+(s[j]-'0');
-            }
-            for(j=0; j<z; j++)
-            {
-                scanf("%d",&y);
-                v[x].pb(y);
-                v[y].pb(x);
-            }
+            int x, y; scanf("%d%d", &x, &y);
+            g[x].push_back(y), g[y].push_back(x);
         }
-        c=0;
-        for(i=0; i<n; i++)
+
+        dfs(0, 0);
+
+        int id=0;
+        for(int i=0; i<n; i++)
         {
-            if(vis[i]==0)
-            {
-                dfs(i,i);
-            }
+            if(!vis[i]) dfs2(i, id++);
         }
-        for(i=0; i<=n; i++)
+
+        for(auto p:E)
         {
-            if(ans[i]==1)
-            {
-                c++;
-            }
+            f[component[p.first]]++;
+            f[component[p.second]]++;
         }
-        printf("Case %d:\n",t++);
-        sort(pr.begin(),pr.end());
-        printf("%d critical links\n",pr.size());
-        for(i=0; i<pr.size(); i++)
-        {
-            printf("%d - %d\n",pr[i].first,pr[i].second);
-        }
-        mem(low);
-        mem(dis);
-        mem(ans);
-        mem(vis);
-        pr.clear();
-        for(i=0; i<=n; i++)
-        {
-            v[i].clear();
-        }
+
+        int ans=0;
+        for(int i=0;i<n;i++) if(f[i]==1) ans++;
+
+        ans=(ans+1)/2;
+
+        printf("Case %d: %d\n",cs++,ans);
     }
     return 0;
 }
-
